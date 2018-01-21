78addb3c79636e739fa7a63e68dec5144c9c256f
alchemist95 <shashank.9d@gmail.com>
2018-01-21 19:03:29 +0530

    [VALIDATION] Uniqueness validation of username and email on server

--- server/shared/validations/signup.js
+++ server/shared/validations/signup.js
@@ -1,7 +1,7 @@
 import Validator from 'validator';
 import isEmpty from 'lodash/isEmpty';
 
-export default function  validateInput(data){
+export default function validateInput(data){
 	let errors = {};
 
 	if (Validator.isEmpty(data.username)){
